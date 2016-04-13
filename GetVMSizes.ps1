14acc3fced1bdf4e5265e07c6aed778d6ab5a33d
Yanivro <spike77707@gmail.com>
2016-04-13 15:30:05 +0300

    Added files via upload

---
+++ GetVMSizes.ps1
@@ -0,0 +1,13 @@
+﻿$azurelocations = Get-AzureLocation
+$out = @()
+foreach ($location in $azurelocations)
+{
+    $VMSizes = $location.VirtualMachineRoleSizes
+    $VMSizesStr = $VMSizes -join ', '
+    $props = @{
+        Name = $location.Name
+        VMSizes = $VMSizesStr}
+    $out += New-Object PsObject -Property $props
+}
+
+$out | Format-Table -AutoSize -Wrap Name, VMSizes
